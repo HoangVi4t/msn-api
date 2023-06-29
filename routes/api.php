12c146662067c37cc7650fa0cad9762084cef798
unknown <hvj4444@gmail.com>
2023-06-29 18:25:20 +0700

    fixed

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 
+
+
 Route::post('/register', [\App\Http\Controllers\API\AuthController::class, 'register']);
 Route::post('/login', [\App\Http\Controllers\API\AuthController::class, 'login']);
 
